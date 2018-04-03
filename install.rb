3925456d2e316299d528713aa9adae9eebcba193
Gazaunga <Gazaunga@gmail.com>
2018-04-02 22:56:17 -0400

    Update install.rb

--- install.rb
+++ install.rb
@@ -7,29 +7,29 @@ SOURCE = "#{Dir.home}/openbox-dt"
 DESTINATION = "#{Dir.home}/"
 
 apps = %w[
-  openbox 
-  tint2-git 
-  compton-git 
+  openbox
+  tint2-git
+  compton-git
   vlc
-  ranger 
-  mpd 
-  ncmpcpp 
-  rxvt-unicode-pixbuf 
-  dunst 
-  obmenu-generator 
-  sxhkd-git 
-  ob-autostart 
-  obkey 
-  latex-mk 
-  j4-dmenu-desktop 
-  networkmanager-git 
-  siji-git 
-  ttf-font-awesome-4 
-  zathura-git 
-  zathura-cb-git 
-  zathura-ps-git 
-  zathura-djvu-git 
-  zathura-pdf-poppler-git
+  ranger
+  mpd
+  ncmpcpp
+  rxvt-unicode-pixbuf
+  dunst
+  obmenu-generator
+  sxhkd-git
+  ob-autostart
+  obkey
+  latex-mk
+  j4-dmenu-desktop
+  networkmanager-git
+  siji-git
+  ttf-font-awesome
+  zathura
+  zathura-cb
+  zathura-ps
+  zathura-djvu
+  zathura-pdf-poppler
 ]
 
 install = ->app{ `trizen -Syu --noconfirm --ignore="inxi" #{app}` }
